e100ad558ffec31566e8c12c679c59163cdbda3a
Johnson-zx <johnson.p0118@gmail.com>
2021-10-17 14:01:37 -0700

    Currency Scraper

--- MicroService Demo/socket/Server/index.js
+++ MicroService Demo/socket/Server/index.js
@@ -3,7 +3,7 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 361 });
 
 wss.on("connection", ws => {
-    
+
     ws.on("message", data => {
         console.log(`Client to Server: ${data}`);
         ws.send(data.toString().toUpperCase());
